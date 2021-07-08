d36a484c64c24de8c392a065469f7b9e183a7feb
Sherlock <tiennguyenhust@.com>
2021-07-08 16:40:30 +0200

    .

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # gpt-2
+Innovation week
---
+++ app.py
@@ -0,0 +1,82 @@
+"""
+You need to run the app from the root
+To run the app
+$ streamlit run app.py
+"""
+
+import fire
+import json
+import os
+import numpy as np
+import tensorflow.compat.v1 as tf
+
+import model, sample, encoder
+
+import streamlit as st
+from PIL import Image
+
+image = Image.open('logo-epita.png')
+st.image(image, caption='*')
+
+key_words = st.text_input('Input your key words:')
+
+def generate_paper(
+    key_words,
+    model_name='paper',
+    seed=None,
+    nsamples=1,
+    batch_size=1,
+    length=None,
+    temperature=1,
+    top_k=0,
+    top_p=1,
+    models_dir='models',
+    ):
+    
+    models_dir = os.path.expanduser(os.path.expandvars(models_dir))
+    if batch_size is None:
+        batch_size = 1
+    assert nsamples % batch_size == 0
+
+    enc = encoder.get_encoder(model_name, models_dir)
+    hparams = model.default_hparams()
+    with open(os.path.join(models_dir, model_name, 'hparams.json')) as f:
+        hparams.override_from_dict(json.load(f))
+
+    if length is None:
+        length = hparams.n_ctx // 2
+    elif length > hparams.n_ctx:
+        raise ValueError("Can't get samples longer than window size: %s" % hparams.n_ctx)
+
+    with tf.Session(graph=tf.Graph()) as sess:
+        context = tf.placeholder(tf.int32, [batch_size, None])
+        np.random.seed(seed)
+        tf.set_random_seed(seed)
+        output = sample.sample_sequence(
+            hparams=hparams, length=length,
+            context=context,
+            batch_size=batch_size,
+            temperature=temperature, top_k=top_k, top_p=top_p
+        )
+
+        saver = tf.train.Saver()
+        ckpt = tf.train.latest_checkpoint(os.path.join(models_dir, model_name))
+        saver.restore(sess, ckpt)
+
+        if not key_words:
+            st.warning('You need to enter key words')
+        context_tokens = enc.encode(key_words)
+        generated = 0
+        for _ in range(nsamples // batch_size):
+            out = sess.run(output, feed_dict={
+                context: [context_tokens for _ in range(batch_size)]
+            })[:, len(context_tokens):]
+            for i in range(batch_size):
+                generated += 1
+                text = enc.decode(out[i])
+                
+    return text
+
+if key_words:  
+    paper = generate_paper(key_words)
+    st.markdown(paper)
